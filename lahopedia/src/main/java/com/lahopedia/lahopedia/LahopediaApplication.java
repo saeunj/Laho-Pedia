6cd13f514c9b81e63fd6b2c8fe5fd820a9debb7a
BJuyoung <qkdwn0313@gmail.com>
2021-05-16 21:40:20 +0900

    Initial commit

---
+++ lahopedia/src/main/java/com/lahopedia/lahopedia/LahopediaApplication.java
@@ -0,0 +1,13 @@
+package com.lahopedia.lahopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LahopediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LahopediaApplication.class, args);
+    }
+
+}
---
+++ lahopedia/src/test/java/com/lahopedia/lahopedia/LahopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lahopedia.lahopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LahopediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
